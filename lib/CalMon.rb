a4f37f8aa44e557c57f949cd23a6a83e46499e99
Randy D. Wallace Jr <randy@randywallace.com>
2012-04-30 17:58:44 -0400

    move some code around to satisfy the library

--- lib/CalMon.rb
+++ lib/CalMon.rb
@@ -1,28 +1,10 @@
 #!/usr/bin/env ruby
 
-$:.unshift File.dirname(__FILE__)
-
+require 'rubygems'
+require "jimson"
 require "CalMon/version"
 
 module CalMon
-
 end
 
-require 'rubygems'
 
-require 'jimson'
-
-class Test
-  extend Jimson::Handler
-
-  def sum(a,b,c)
-    a + b + c
-  end
-  
-  def sub(a,b)
-    a - b
-  end
-end
-
-server = Jimson::Server.new(Test.new)
-server.start
