2768e9c32dc10672544c35c122e8c43f15dcecea
Randy D. Wallace Jr <randy@randywallace.com>
2013-02-28 15:21:04 +0000

    Start bringing in ical portion

--- doc/ruby_client.rb
+++ doc/ruby_client.rb
@@ -2,7 +2,37 @@
 
 require 'rubygems'
 require 'jimson'
-client = Jimson::Client.new("http://127.0.0.1:8999")
-puts client.start_time(Time.now, "This is a test")
-puts client.stop_time(Time.now, "This is a test")
-puts client.running_time(Time.now,"This is a test")
+require 'optparse'
+
+def run_client(opts = {})
+  @server = opts.delete(:server) || '127.0.0.1'
+  @port   = opts.delete(:port)   || '8999'
+  client = Jimson::Client.new("http://#{@server}:#{@port}")
+  puts client.start_time(Time.now, "This is a test")
+  puts client.stop_time(Time.now, "This is a test")
+  puts client.running_time(Time.now,"This is a test")
+end
+
+class ParseOptions
+  def self.parse(args)
+    options = {}
+    OptionParser.new do |opt|
+      opt.on '-s SERVER', '--server=SERVER', 'CalMon Hostname/IP' do |val|
+        options[:server] = val
+      end
+      opt.on '-p PORT', '--port=PORT', 'CalMon Port' do |val|
+        options[:port] = val
+      end
+      opt.on_tail '-h', '--help', 'Show Help' do
+        puts opt
+        exit
+      end
+      opt.parse!
+      run_client options 
+    end
+  end
+end
+
+ParseOptions.parse ARGV
+
+
---
+++ lib/CalMon/Calendar/File.rb
@@ -0,0 +1,18 @@
+#
+# Author::    Randy D. Wallace Jr. (mailto:randy@randywallace.com)
+# Copyright:: Copyright (c) 2012 Randy D. Wallace Jr.
+# License::   MIT
+#
+
+require 'ri_cal'
+
+module CalMon
+
+  # This class handles the reading and writing Calendar (icalendar)
+  # events to and from disk
+
+  class File
+
+
+  end
+end
